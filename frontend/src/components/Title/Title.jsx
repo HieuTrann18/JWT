a4d5ddf2aaba1d2e6aa3782263b7452d43c26dd9
HieuTrann18 <hieu.tt.2455@aptechlearning.edu.vn>
2025-10-26 02:08:59 +0700

    update

--- frontend/src/components/Title/Title.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "./style.module.scss";
-const Title = ({ content, level = "1" }) => {
-  const Tag = `h${level}`;
-  return <Tag className={styles.title}>{content}</Tag>;
-};
-
-export default Title;
--- frontend/src/pages/Admin/Dashboard.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Dashboard = () => {
-  return <div>Hello admin</div>;
-};
-
-export default Dashboard;
